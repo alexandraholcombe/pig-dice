6948e380a7e12802dfa072b220c106970da6bbda
Alexandra Holcombe and Elise St Hilaire <alexandra@>
2017-01-25 11:03:41 -0800

    Initialized toggle functionality

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,11 @@ var diceRoll = function() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+var togglePlayerDiv = function() {
+  if (confirm("Turn over, no change to score")) {
+    $("#player-2-container .overlay, #player-1-container .overlay").toggle();
+  }
+}
 //front-end (UI)
 
 $(document).ready(function() {
@@ -26,21 +31,21 @@ $(document).ready(function() {
       $("#p1-current-score").text(player1.currentScore);
       $("#p1-die").text(rollResult);
       setTimeout(function() {
-        alert("Turn over, no change to score");
+        togglePlayerDiv();
       }, 100);
-    } else {
-      player1.currentScore += rollResult;
-      $("#p1-current-score").text(player1.currentScore);
-      $("#p1-die").text(rollResult);
-    }
-  });
-  $(".p1-hold").click(function() {
-    player1.totalScore += player1.currentScore;
-    $("#p1-total-score").text(player1.totalScore);
-    player1.currentScore = 0;
+  } else {
+    player1.currentScore += rollResult;
     $("#p1-current-score").text(player1.currentScore);
-    setTimeout(function() {
-      alert("End of turn. Total Score:" + player1.totalScore);
-    }, 100);
-  });
+    $("#p1-die").text(rollResult);
+  }
+});
+$(".p1-hold").click(function() {
+  player1.totalScore += player1.currentScore;
+  $("#p1-total-score").text(player1.totalScore);
+  player1.currentScore = 0;
+  $("#p1-current-score").text(player1.currentScore);
+  setTimeout(function() {
+    alert("End of turn. Total Score:" + player1.totalScore);
+  }, 100);
+});
 });
