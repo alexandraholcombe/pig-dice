c6aeffec38999cd86e0fa60babba015e7d99dccc
Alexandra Holcombe and Elise St Hilaire <elise@>
2017-01-25 10:22:53 -0800

    Make dice picture change to reflect random number, add roll to current score

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,18 @@ var diceRoll = function() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-
-
 //front-end (UI)
 
 $(document).ready(function() {
+
+  $("#p1-current-score").text(player1.currentScore);
+  $("#p1-total-score").text(player1.totalScore);
+
   $(".p1-roll").click(function() {
     var rollResult = diceRoll();
-    console.log(rollResult);
+    player1.currentScore = player1.currentScore + rollResult;
+    $("#p1-current-score").text(player1.currentScore);
+    $("#p1-die").text(rollResult);
   });
+
 });
