dbfc736cc74df8fa4103c99460c15ee156a18c33
Alexandra Holcombe and Elise St Hilaire <alexandra@>
2017-01-25 11:31:19 -0800

    create winner div, dice starts at value of 0

--- js/scripts.js
+++ js/scripts.js
@@ -11,27 +11,32 @@ var diceRoll = function() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-var togglePlayerDiv = function(string) {
-  setTimeout (function() {
-    if (confirm(string)) {
-      $("#player-2-container .overlay, #player-1-container .overlay").toggle();
-    };
-  }, 100);
+var scoreChecker = function(current, total) {
+  if ((current + total) >= 100) {
+    //pop the winner div
+  }
 }
 //front-end (UI)
 
 $(document).ready(function() {
+  $("#p1-die, #p2-die").text(0);
+  var togglePlayerDiv = function(string) {
+    setTimeout (function() {
+      if (confirm(string)) {
+        $("#player-2-container .overlay, #player-1-container .overlay").toggle();
+      };
+    }, 100);
+  }
 
   $("#p1-current-score").text(player1.currentScore);
   $("#p1-total-score").text(player1.totalScore);
-
   $(".p1-roll").click(function() {
     $("#player-2-container .overlay").show();
     var rollResult = diceRoll();
     if (rollResult === 1) {
       player1.currentScore = 0;
       $("#p1-current-score").text(player1.currentScore);
-      $("#p1-die").text(rollResult);
+      $("#p1-die").text("J");
       togglePlayerDiv ("Turn over, no change to total score");
     } else {
       player1.currentScore += rollResult;
